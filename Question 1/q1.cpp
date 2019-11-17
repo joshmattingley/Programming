13bb22b1d9f6f91c558ba969b3fa8b2f56e77ae6
joshmattingley <joshmattingley@users.noreply.github.com>
2019-11-17 22:23:49 +0000

    Uploaded!

--- Question 1/q1.cpp
+++ Question 1/q1.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <bits/stdc++.h>
 
-  void line() {
+  void line_small() {
     std::cout << "==============================\n";
+  }
+
+  void line_big() {
+    std::cout << "\n===============================================\n" << std::endl;
   }
 
   float time(float d, float s) {
@@ -11,6 +16,21 @@
       //declaring the operations inside the function
       result = d / s;
       return(result);
+  }
+
+  float Kilo(float n) {
+    float result;
+    //declaring the operations inside the function
+    result = n * 10000;
+    return(result);
+  }
+
+  //function to convert hours to seconds
+  float seconds(float n) {
+    float result;
+    //declaring the operations inside the function
+    result = n * pow(60, 2);
+    return(result);
   }
 
 int main () {
@@ -23,7 +43,7 @@ int main () {
     float Speed[] = {6.2, 52.8, 18.3};
 
     //Defining the array of disciplines
-    char Disc[3][12] = {"Swimming", " Cylcing", " Running"};
+    char Disc[3][12] = {"Swimming", " Cycling", " Running"};
 
     //Distance variables
     float Swim_Dist, Cyc_Dist, Run_Dist;
@@ -42,11 +62,12 @@ int main () {
 
     //This is the friendly message, output to the user recalling their values.
     for (int i = 0; i < 3; i++) {
-      std::cout << "For " << Disc[i] << " distance, you entered: " << Dist[i] << " meters\n";
+      std::cout << "For " << Disc[i] << " distance, you entered: " << Kilo(Dist[i]) << " meters.\n";
     }
 
     //This sums up the times to complete each section.
     float total = time(Dist[0], Speed[0]) + time(Dist[1], Speed[1]) + time(Dist[2], Speed[2]);
+
 
     //This if statement forces the user to enter positive integers for the distances
     if (Dist[0] < 0 || Dist[1] < 0 || Dist [2] < 0){
@@ -55,18 +76,18 @@ int main () {
       else {
         //Setting up heading of the title
         std::cout << "\n" << "Discipline\t" << "Time Taken (s)\n";
-        line();
+        line_small();
 
         //This loop outputs our discipline and the accompanying time taken, it also sets the decimal places
         for (int i = 0; i < 3; i++) {
             std::cout << std::setprecision(3);
-            std::cout << Disc[i] << " \t" << time(Dist[i], Speed[i]) * pow(60, 2) <<"\n";
+            std::cout << Disc[i] << " \t" << seconds(time(Dist[i], Speed[i])) <<"\n";
               }
         }
 
     //This tells the user the total time
     std::cout << "\n" << std::setprecision(2);
     //This outputs the total time
-    std::cout << "Therefore, the total time would be " << total << " Hours";
+    std::cout << "Therefore, the total time would be " << total << " hours.";
 
-}
+  }
--- Question 2/q2_Fred_Modified.cpp
+++ Question 2/q2_Fred_Modified.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <bits/stdc++.h>
 
   void line_small() {
     std::cout << "==============================\n";
@@ -15,6 +16,21 @@
       //declaring the operations inside the function
       result = d / s;
       return(result);
+  }
+
+  float Kilo(float n) {
+    float result;
+    //declaring the operations inside the function
+    result = n * 10000;
+    return(result);
+  }
+
+  //function to convert hours to seconds
+  float seconds(float n) {
+    float result;
+    //declaring the operations inside the function
+    result = n * pow(60, 2);
+    return(result);
   }
 
 int main () {
@@ -46,11 +62,12 @@ int main () {
 
     //This is the friendly message, output to the user recalling their values.
     for (int i = 0; i < 3; i++) {
-      std::cout << "For " << Disc[i] << " distance, you entered: " << Dist[i] * 1000 << " meters.\n";
+      std::cout << "For " << Disc[i] << " distance, you entered: " << Kilo(Dist[i]) << " meters.\n";
     }
 
     //This sums up the times to complete each section.
     float total = time(Dist[0], Speed[0]) + time(Dist[1], Speed[1]) + time(Dist[2], Speed[2]);
+
 
     //This if statement forces the user to enter positive integers for the distances
     if (Dist[0] < 0 || Dist[1] < 0 || Dist [2] < 0){
@@ -64,7 +81,7 @@ int main () {
         //This loop outputs our discipline and the accompanying time taken, it also sets the decimal places
         for (int i = 0; i < 3; i++) {
             std::cout << std::setprecision(3);
-            std::cout << Disc[i] << " \t" << time(Dist[i], Speed[i]) * pow(60, 2) <<"\n";
+            std::cout << Disc[i] << " \t" << seconds(time(Dist[i], Speed[i])) <<"\n";
               }
         }
 
@@ -133,31 +150,32 @@ int main () {
 
     // caluating the combinations (times) of each item of technical clothing (didne use an array, this way is clearer)
     float combinations[6];
-    //Cycling Shoes and Goggles
-    combinations[0] = ((technical_clothing_time[0] + technical_clothing_time[3]) / 2);
     //Cycling Shoes and Sunglasses
-    combinations[1] = ((technical_clothing_time[0] + technical_clothing_time[4]) / 2);
-    //Running Shoes and Goggles
-    combinations[2] = ((technical_clothing_time[1] + technical_clothing_time[3]) / 2);
+    combinations[0] = ((technical_clothing_time[0] + technical_clothing_time[4]) / 2);
     //Running Shoes and Sunglasses
-    combinations[3] = ((technical_clothing_time[1] + technical_clothing_time[4]) / 2);
-    //Flippers and Goggles
-    combinations[4] = ((technical_clothing_time[2] + technical_clothing_time[3]) / 2);
+    combinations[1] = ((technical_clothing_time[1] + technical_clothing_time[4]) / 2);
+    //Cycling Shoes and Goggles
+    combinations[2] = ((technical_clothing_time[0] + technical_clothing_time[3]) / 2);
+    //Running Shoes and Goggles
+    combinations[3] = ((technical_clothing_time[1] + technical_clothing_time[3]) / 2);
     //Flippers and Sunglasses
-    combinations[5] = ((technical_clothing_time[2] + technical_clothing_time[4]) / 2);
+    combinations[4] = ((technical_clothing_time[2] + technical_clothing_time[4]) / 2);
+    //Flippers and Goggles
+    combinations[5] = ((technical_clothing_time[2] + technical_clothing_time[3]) / 2);
 
         line_big();
-        
+
         //sets 3 d.p.
         std::cout << "\n" << std::setprecision(3);
 
         std::cout << "Options" << "\t\t\t\t" << "|" << "\t" << "Time taken (hours)" << std::endl;
         std::cout << "================================|==========================" << std::endl;
 
-        char options[6][50] = {"Cycling shoes & Goggles\t", "Cycling shoes & Sunglasses", "Running shoes & Goggles\t", "Running shoes & Sunglasses", "Flippers & Goggles\t", "Flippers & Sunglasses\t"};
+        char options[6][50] = {"Cycling shoes & Sunglasses", "Running shoes & Sunglasses", "Cycling shoes & Goggles\t", "Running shoes & Goggles\t", "Flippers & Sunglasses\t", "Flippers & Goggles\t"};
 
+
+        //This loop outputs all of the calculations of time with each combination
         for(int i = 0; i < 6; i++) {
           std::cout << options[i] << "\t|\t"  << combinations[i] << std::endl;
         }
-
 }
